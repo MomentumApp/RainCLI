271507b09f759f4f836b6f0a8131ed1db4ce2de4
David Boskovic <david@momentumapp.co>
2013-04-18 17:23:47 -0600

    sample config file

---
+++ config.js
@@ -0,0 +1,7 @@
+var conf = {
+	momentum_domain : "", 		// http://www.yourdomain.com -> must point to url on Momentum's servers
+	momentum_theme: "",			// the name of the default them you'll be working with
+	momentum_apiuser: "",		// your api username
+	momentum_apikey: ""			// the api key for user
+}
+module.exports = conf;
